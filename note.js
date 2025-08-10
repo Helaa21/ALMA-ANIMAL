d082dd6750f63d61741744a60460e941f705eb1e
Helaa21 <arianajuliahuarcaya@gmail.com>
2025-08-09 21:16:03 -0500

    Update and rename script.js to note.js

---
+++ note.js
@@ -0,0 +1,20 @@
+{
+  "type": "service_account",
+  "project_id": "alma-animalperu",
+  "private_key_id": "1371b837e744bc77bdf491050ed7438873f69e89",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCUE8NbKhsmeP04\nXAoSqpR6cW9jH4wrFjWsaqp2JEvqhOZf9eBQFCnDsR0gBKA7Wj++uBVtxQkNdYW0\nsFE0663Y8nWXtpG+qrSQBmA+30XUo27/FLhyP/fzNWwHxtBzY1Fke1Bw//HdXahH\nOXFqRVoFzUzFFpW8eAjW9bXZ+u/17GXWA7zOMjopLECs4TxlXjyYBZHpDT0P3J4m\nmqX28YcIzkbll2dII8ruV/rPFsj4JoKRL4KTPOEe4fkh6frnLu6QB1x7xEsgXKr1\n/ED/wXGoMNHQQjL26O+IxYGhmEpcn45PN6NJhR0r0WSDZ6aBhC+lCicCSmZMOGVE\nrik5l1crAgMBAAECggEACdARdx4gNHl6Fl0LEdeaBU81GXWCvWw4XFepOjEHkuAB\nwEQz/DqJ1f0LuA52a9h8NvBOAzs2cVzjYmj4yLABkknm/wnLOL5pPs4dXK1BHGlE\nNsUxv+h7TvOwWAeV0r+3Khh96S8c4RWOzEGDQY6c+5tyRfFMNPQjadNYnLxjYIGT\nHw3VxuHiQ08v9D2a0r8XRM/Ttmy2x1TLFLoux6Rwuc0681mMsc2kOibhFcvX0H/0\nqBaNHCbgteZZ++yqtm2IjNixF7p6A9cHT9tmEgMon/OQ+/JVc/pVOEHO4u0e33y2\nPvGrh6sYBxl5IkgJEw4CT/bZpJHpuUgN5tyfpqmZYQKBgQDKXGZh1mTTlMDeCWVI\nk1hao6frCrb+LFxdDHfxg8R/SHlI3JBT7BP63UMPes7EYUCB5VzyTMW8z9XWpnHS\n6zuWgatYHpRZObOWxZzP9wx3Zg0Vg/PjJZNRXqTsWAoPNFBUH34xqtNiekyZSuUo\nBVHZ/s8eCijc7CBPS8+8uQwNEQKBgQC7U9VN9St4Ez5V4ZRpOBAFNav1mtG5bv25\njk0stXn9xncEuImjG1Hjh+jQaQjK3JXi9clqVPd5YVEbkcV0fWFWYhtMbfQDNlok\nWMR6cOixsKaoOfSDq9uW0g+oSI2YMku00C3GL8f4OJeVawQYF3Q2w3U8we1pW0YN\nbhSQgDwQewKBgQC0uBA3D+C7k/0qTfnF9tDRIZa0sbz7AWL7NkdnyOWTRoYKK/A5\nAnxVbYSWLna5qqCRYOTA/w8YsAOozryYQLvWjbcX2zdg98cKvzBsCZV4jArjTBRi\npwwJfxyKdZWGPpH+jqGEeaqOJ+qAfAJq21Vd+dG+YhRcOAQizPGY+SlqAQKBgQCf\nv04XvQvkygROmKBHWlfPGYYbXHuc9tnIc2G8zsXJdIXqsLQvNA0RS8VudSPaW2I2\nugOZefvQtvzD1fvc608mq2M8OkfstV0JwelLwR2u6kY1D7dE0jng1CImEDkvoTSO\nbTmb2QNe2mPqN+i14spoKSbBOV+MiRmkOkuvLqwKIQKBgEPaJttCt5TXt0uE8smO\nGQfjEJbL9svRndO9/ZiRN4Br2l2HkaI56nLEMlpsNgINblp36HwlaZEjG7zMgYCV\nTLv6JovHv9B1h4xu/WpLonHXSnemZA3DDUa4+WEMXIVRbGcR70UQBX0d7qezyaq4\nZUvwHTD4O6e0LHH6qflwgh01\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@alma-animalperu.iam.gserviceaccount.com",
+  "client_id": "117713203121534586263",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40alma-animalperu.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
+
+
+
+
+
+
+
